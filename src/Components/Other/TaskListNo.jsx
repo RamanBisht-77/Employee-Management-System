a36c58e2cf7935faf5eb092d22994dc343ecab94
Raman <ramanbisht77@gmail.com>
2026-05-25 23:09:08 +0530

    made website responsive

--- src/Components/Other/TaskListNo.jsx
+++ src/Components/Other/TaskListNo.jsx
@@ -10,30 +10,30 @@ const TaskListNo = ({ employeeData }) => {
   console.log(currentEmployee);
 
   return (
-    <div className="flex gap-4 bg-[#1c1c1c] p-5 mt-5 max-[700px]:flex-wrap justify-center">
+    <div className="flex gap-4 bg-[#1c1c1c] rounded-xl p-5 mt-5 max-[700px]:grid grid-cols-2">
 
-      <div className="w-[50%] rounded-xl py-6 px-9 bg-red-500">
+      <div className="w-[50%] rounded-xl py-6 px-9 bg-red-500 max-[700px]:w-[100%]">
         <h2 className="text-3xl font-semibold">
           {currentEmployee?.taskCounts.newTask}
         </h2>
         <h3 className="text-xl font-medium">New Task</h3>
       </div>
 
-      <div className="w-[50%] rounded-xl py-6 px-9 bg-blue-500">
+      <div className="w-[50%] rounded-xl py-6 px-9 bg-blue-500 max-[700px]:w-[100%]">
         <h2 className="text-3xl font-semibold">
           {currentEmployee?.taskCounts.completed}
         </h2>
         <h3 className="text-xl font-medium">Completed Task</h3>
       </div>
 
-      <div className="w-[50%] rounded-xl py-6 px-9 bg-green-500">
+      <div className="w-[50%] rounded-xl py-6 px-9 bg-green-500 max-[700px]:w-[100%]">
         <h2 className="text-3xl font-semibold">
           {currentEmployee?.taskCounts.active}
         </h2>
         <h3 className="text-xl font-medium">Active Task</h3>
       </div>
 
-      <div className="w-[50%] rounded-xl py-6 px-9 bg-pink-500">
+      <div className="w-[50%] rounded-xl py-6 px-9 bg-pink-500 max-[700px]:w-[100%]">
         <h2 className="text-3xl font-semibold">
           {currentEmployee?.taskCounts.failed}
         </h2>
